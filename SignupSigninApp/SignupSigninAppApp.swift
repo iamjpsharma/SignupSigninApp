278da5fb31382eb0b63c454a611ff327bf39e96b
Jai Prakash sharma <sjaiprakash457@gmail.com>
2025-02-10 14:43:41 +0530

    Initial Commit

---
+++ SignupSigninApp/SignupSigninAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignupSigninAppApp.swift
+//  SignupSigninApp
+//
+//  Created by jaiprakash sharma on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SignupSigninAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
